a64d99cc71fa2cdb6c6dfb9f35a926e9aacaed81
MahirAbsar <mahirabsar181@gmail.com>
2025-05-09 10:53:36 +0600

    init next js app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-violet-500 font-black">
+        Welcome to next js
+      </h1>
+    </div>
+  );
+};
+export default Home;
